a74d4c648a7bfdd4ef843d844706101bc3e1117f
Drew Langley <TheZimbo16@users.noreply.github.com>
2017-04-20 17:37:47 +0200

    Fixes for integration.

--- reader.py
+++ reader.py
@@ -4,6 +4,7 @@
 import sys
 import nsq
 import json
+import tornado.ioloop
 
 URI = "localhost:27017"
 
@@ -22,6 +23,27 @@ def requestHandler(message):
 
             if query == 'insert':
                 insertOne(obj['content']['fname'], obj['content']['sname'], obj['content']['email'], obj['content']['password'], obj['content']['stud_num'], obj['content']['phone'])
+                request="""{
+                        "src"  : "users",
+                        "dest" : "notification",
+                        "msgType" : "request",
+                        "queryType" : "insert",
+                        "content" : {
+                                        "email" : "obj['content']['email']",
+                                    }
+                }""";
+
+                def pub_message():
+                     writer.pub('notifications',request.encode('utf-8'), finish_pub)
+
+                def finish_pub(conn, data):
+                    #print(data)
+                    tornado.ioloop.IOLoop.current().stop()
+
+
+                writer = nsq.Writer(['127.0.0.1:4150'])
+                tornado.ioloop.PeriodicCallback(pub_message,1000).start()
+                nsq.run()
 
             elif query == 'update':
                 update(obj['content']['update'], obj['content']['fname'], obj['content']['sname'], obj['content']['email'], obj['content']['password'], obj['content']['stud_num'], obj['content']['phone'])
@@ -96,9 +118,13 @@ def delete(toDelete):
 
 ############################################### Function for getUser from mongo db ######################################################
 def getUser(toGet):
-        user = db.userDB.find_one({"StudentNumber":toGet})
-        if(user["StudentNumber"] == toGet):
-            print(user)
+        if(db.userDB.find_one({"StudentNumber":toGet})):
+            print(db.userDB.find_one({"StudentNumber":toGet}))
+            return True
+
+        else:
+            #print("User does not exist")
+            return False
 
 ############################################ Function to set user as an Admin ###############################################################
 def makeAdmin(UserName):
